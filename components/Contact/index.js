8e8db9594283abdc4156311fb024d69d2247fe1c
Maxwel <maxwelsant96@icloud.com>
2021-10-15 09:56:18 -0300

    contact section

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -1,7 +1,28 @@
 import React from "react";
+import styles from "./Contact.module.css";
+import Separator from "../common/Separator";
+import SocialContact from "../common/SocialContact";
+// import resume from "../../public/resume-max.pdf";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className={styles.contact}>
+      <Separator />
+      <label className={styles.sectionTitle}>Contact</label>
+      <div className={styles.contactContainer}>
+        <div className={styles.contactLeft}>
+          <p>Want to get in touch? Contact me on any of the platform</p>
+          <SocialContact />
+        </div>
+        <div className={styles.download}>
+          <a href="./resume-max.pdf" download>
+            <i className={`fi fi-rr-cloud-download ${styles.downloadIcon}`}></i>
+            Download Resume
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
